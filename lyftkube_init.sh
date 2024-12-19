1ad60f8ec3dd2f76c3c81f541e7135c457e3ac4c
Shinnosuke Okada <sokada@lyft.com>
2024-12-19 11:03:54 -0500

    generalize

--- lyftkube_init.sh
+++ lyftkube_init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Author: @mikecutalo
 
+EMAIL_ADDRESS='sokada@lyft.com'
+
 rm -rf ~/.kube/clusters
 mkdir -p ~/.kube/clusters
 
-lyftkube get clusters -e production | cslice 0 1 | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email mcutalo@lyft.com --cluster={} > ~/.kube/clusters/{}"
-lyftkube get clusters -e staging | cslice 0 1 | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email mcutalo@lyft.com --cluster={} > ~/.kube/clusters/{}"
+lyftkube get clusters -e production | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
+lyftkube get clusters -e staging | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
 
-ls ~/.kube/clusters | xargs -L 1 -I{} echo "alias {}='kubectl --kubeconfig /Users/mcutalo/.kube/clusters/{}'"
+ls ~/.kube/clusters | xargs -L 1 -I{} echo "alias {}='kubectl --kubeconfig ~/.kube/clusters/{}'"
