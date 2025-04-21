08fe4ad60a9a01ea668268a923c10c33e9a3b471
Shinnosuke Okada <sokada@lyft.com>
2025-04-21 15:49:44 -0400

    Include all clusters

--- lyftkube_alias.sh
+++ lyftkube_alias.sh
@@ -6,7 +6,9 @@ EMAIL_ADDRESS='sokada@lyft.com'
 rm -rf ~/.kube/clusters
 mkdir -p ~/.kube/clusters
 
-lyftkube get clusters -e production | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
-lyftkube get clusters -e staging | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
+#lyftkube get clusters -e production | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
+#lyftkube get clusters -e staging | awk 'NR>1 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
+
+lyftkube get clusters | awk 'NR>2 {print $1}' | xargs -L 1 -I{} sh -c "lyftkube kubeconfig --email ${EMAIL_ADDRESS} --cluster={} > ~/.kube/clusters/{}"
 
 ls ~/.kube/clusters | xargs -L 1 -I{} echo "alias {}='kubectl --kubeconfig ~/.kube/clusters/{}'"
